8cc0935d140aba213edee2b8fdefa218683c5789
Leo Vigna <leo.vigna@gmail.com>
2020-10-06 16:57:21 +0200

    Truffle migration with typechain

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  contracts_directory: "./src",
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+  },
+  compilers: {
+    solc: {
+      version: ">=0.5.0", // A version or constraint - Ex. "^0.5.0"
+      //parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200   // Optimize for how many times you intend to run the code
+        },
+        evmVersion: "istanbul"
+      }
+    }
+  }
+}
